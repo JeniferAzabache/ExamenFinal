3c38d1f202ee896ec76eecddb7136d61858fa9b6
LENOVO <LENOVO@DESKTOP-E8SK85M>
2020-03-14 12:26:18 -0500

    Procesando

--- ExamenFinalAGJG/WebApplication/Controllers/ClienteController.cs
+++ ExamenFinalAGJG/WebApplication/Controllers/ClienteController.cs
@@ -14,31 +14,15 @@ namespace WebApplication.Controllers
         public ActionResult Index()
         {
             try
-            {
-                //string sql =
-                //   @"select cc.idContactabilidad, col.nombreColaborador, cli.nombreCliente, pro.nombreProducto,fechaContactoCliente
-                //        from tb_ContactoCliente cc inner join tb_Colaborador col
-                //        on cc.idColaborador =col.idColaborador inner join tb_Cliente cli
-                //        on cc.idCliente = cli.idCliente inner join tb_Producto pro
-                //        on cc.idProducto = pro.idProducto";
-
-
+            { 
                 List<tbCliente> listaClientes;
                 using (var db = new BD_ExamenFinalEntities())
                 {
-                    var lista = Select IdCliente, Nombre, NumDocumento from tbCliente where Estado = 'A';
-                               
-                                select new ClienteBEAN
-                                {
-                                   
-                                };
+                    listaClientes = db.tbCliente.ToList();
 
-                    //List < tb_ContactoCliente > listContac = db.tb_ContactoCliente.ToList();
-                    List<ClienteBEAN> listContac = lista.ToList();
-                    return View(listContac);
 
-                    //return View(db.Database.SqlQuery<ClienteBEAN>(sql).ToList());
-                }
+                    return View(listaClientes);
+                 }
             }
             catch (Exception)
             {
