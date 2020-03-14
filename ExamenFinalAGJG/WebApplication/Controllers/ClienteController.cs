d36aa9fa0a0ec63f5ba666ad21471b80d6a68932
LENOVO <LENOVO@DESKTOP-E8SK85M>
2020-03-14 11:31:26 -0500

    En proceso

---
+++ ExamenFinalAGJG/WebApplication/Controllers/ClienteController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Index()
+        {
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return View();
+        }
+    }
+}
